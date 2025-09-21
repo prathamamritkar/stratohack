d2c15ced9dbe96a51fad572ade6414649ddbbcf2
Pratham Amritkar <prathamamritkar@gmail.com>
2025-09-21 10:37:48 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/app/predict-delays/page.tsx
+++ src/app/predict-delays/page.tsx
@@ -1,6 +1,6 @@
 import { getAirportData } from '@/lib/data-loader';
 import { initializeAirportData } from '@/lib/airport-coordinates';
-import { PredictDelaysClientPage } from './predict-delays-client-page';
+import { PredictDelaysClientPage } from '../predict-delays-client-page';
 
 export default async function PredictDelaysPage() {
   const airportData = await getAirportData();
